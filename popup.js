43daf896a0471dc5cec53094ee1534d6f722d448
Matt <1089327+mattcuk@users.noreply.github.com>
2024-02-14 21:06:19 +0000

    Searching bookmarks and history test

--- popup.js
+++ popup.js
@@ -30,3 +30,53 @@ $('#setVar2').on('click', function() {
         $('#response').html('Variable set2!');
     })
 });
+
+$('#searchBoth').on('click', function() {
+    var search = $('#search').val();
+    searchBookmarks(search);
+    searchHistory(search);
+});
+
+$('#searchBookmarks').on('click', function() {
+    var search = $('#search').val();
+    searchBookmarks(search);
+});
+
+$('#searchHistory').on('click', function() {
+    var search = $('#search').val();
+    searchHistory(search);
+});
+
+function searchBookmarks(search) {
+    var bookmarkList = $('#bookmarks');
+    bookmarkList.empty();
+    chrome.bookmarks.search(search, function(results) {
+        console.log(results.length + ' bookmarks found');
+        results.forEach(function(bookmark) {
+            bookmarkList.append($('<option>', {
+                value: bookmark.url,
+                text: bookmark.url
+            }));
+            // console.log('ID: ' + bookmark.id);
+            // console.log('Title: ' + bookmark.title);
+            // console.log('URL: ' + bookmark.url);
+        });
+    });
+}
+
+function searchHistory(search) {
+    var historyList = $('#history');
+    historyList.empty();
+    chrome.history.search({text: 'GitHub', startTime: 0}, function(results) {
+        results.forEach(function(result) {
+            historyList.append($('<option>', {
+                value: result.url,
+                text: result.visitCount + " " + result.url
+            }));
+            // console.log('URL: ' + result.url);
+            // console.log('Last visited: ' + new Date(result.lastVisitTime));
+            // console.log('Visit count: ' + result.visitCount);
+        });
+    });
+}
+
